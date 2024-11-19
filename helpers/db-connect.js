144a0a61c1f6efd4e71a1d185a7b3ad76cf29872
ssinarola <ssi@narola.email>
2024-11-19 18:57:32 +0530

    fix: cleanup

--- helpers/db-connect.js
+++ helpers/db-connect.js
@@ -1,22 +1,3 @@
-// import mongoose from "mongoose";
-
-// function dbConnect() {
-//     mongoose.set('strictQuery', true)
-//     if (mongoose.connections[0].readyState) {
-//         console.log('Already connected with database');
-//         return
-//     }
-//     mongoose.connect(process.env.MONGODB_URL)
-//     mongoose.connection.on('connected', () => {
-//         console.log("Connected with database.")
-//     })
-//     mongoose.connection.on('error', (error) => {
-//         console.log("Error while connecting", error)
-//     })
-// }
-
-
-// export default dbConnect
 
 import mongoose from 'mongoose';
 
